1efe4a01ecabda6caf7092b1064116296e26df66
kamikaonashi <thorschi93@gmx.at>
2023-12-26 07:56:00 +0100

    stone: make prebuilt kernel optional
    in preparation for oss kernel

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -22,6 +22,9 @@ TARGET_FACE_UNLOCK_SUPPORTED := true
 # Charging Animation
 TARGET_USE_PIXEL_CHARGER := true
 
+# Use Prebuilt Kernel
+PREBUILT_KERNEL := true
+
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_stone
 PRODUCT_DEVICE := stone
