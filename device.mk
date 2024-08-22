ab1e576fab481627530e7a8be78ca1fad5bcee99
Sugakesshaa <sugakesshaa@gmail.com>
2024-08-22 20:30:05 +0200

    stone: switch to pixel-libperfmgr

--- device.mk
+++ device.mk
@@ -445,7 +445,7 @@ endif
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.lineage-libperfmgr \
+    android.hardware.power-service.pixel-libperfmgr \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
@@ -546,7 +546,6 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi \
     hardware/google/interfaces \
     hardware/google/pixel \
-     hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
