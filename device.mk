329c9492bd07ad601fadc5ebfbcda241cccc7d74
kamikaonashi <thorschi93@gmx.at>
2024-08-25 08:58:28 +0200

    stone: switch back to lineage libperfmgr

--- device.mk
+++ device.mk
@@ -441,7 +441,7 @@ endif
 
 # Power
 PRODUCT_PACKAGES += \
-    android.hardware.power-service.pixel-libperfmgr \
+    android.hardware.power-service.lineage-libperfmgr \
     libqti-perfd-client
 
 PRODUCT_COPY_FILES += \
@@ -537,6 +537,7 @@ PRODUCT_SOONG_NAMESPACES += \
     hardware/xiaomi \
     hardware/google/interfaces \
     hardware/google/pixel \
+    hardware/lineage/interfaces/power-libperfmgr \
     hardware/qcom-caf/common/libqti-perfd-client
 
 # Telephony
