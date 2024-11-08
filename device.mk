2f91e4e0a29bda8cd970665c4343860a887c1ec0
Tim Zimmermann <tim@linux4.de>
2024-11-08 11:02:23 +0000

    stone: Switch to AIDL Fingerprint HAL

--- device.mk
+++ device.mk
@@ -196,7 +196,8 @@ PRODUCT_PACKAGES += \
 
 # Fingerprint
 PRODUCT_PACKAGES += \
-    android.hardware.biometrics.fingerprint@2.3-service.xiaomi \
+    android.hardware.biometrics.fingerprint-service.xiaomi \
+    android.hardware.biometrics.fingerprint@2.1.vendor \
     vendor.goodix.hardware.biometrics.fingerprint@2.1.vendor \
     libvendor.goodix.hardware.biometrics.fingerprint@2.1.vendor
 
