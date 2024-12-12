714134c273f1957a289ac2b442dd3ae6c113ce2d
kamikaonashi <thorschi93@gmx.at>
2024-12-12 20:28:27 +0100

    stone: add dt2w gesture support

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -182,6 +182,9 @@ BOARD_VENDOR := xiaomi
 BOARD_USES_QCOM_HARDWARE := true
 TARGET_BOARD_PLATFORM := holi
 
+# Power
+TARGET_TAP_TO_WAKE_NODE := /sys/devices/platform/soc/4c88000.i2c/i2c-2/2-0038/fts_gesture_mode
+
 # Properties
 TARGET_ODM_PROP += $(DEVICE_PATH)/properties/odm.prop
 TARGET_PRODUCT_PROP += $(DEVICE_PATH)/properties/product.prop
