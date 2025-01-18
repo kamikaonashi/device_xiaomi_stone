c6d0c1163471b6f4caa012fe146f499de8208742
kamikaonashi <thorschi93@gmx.at>
2025-01-18 17:10:39 +0100

    stone: update device fingerprints

--- lineage_stone.mk
+++ lineage_stone.mk
@@ -23,6 +23,6 @@ PRODUCT_DEVICE := stone
 PRODUCT_MANUFACTURER := Xiaomi
 PRODUCT_BRAND := Xiaomi
 
-BuildFingerprint=POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.10.0.UMPMIXM:user/release-keys
+BuildFingerprint=POCO/moonstone_p_global/moonstone:14/UKQ1.231003.002/V816.0.13.0.UMPMIXM:user/release-keys
 
 PRODUCT_GMS_CLIENTID_BASE := android-xiaomi
