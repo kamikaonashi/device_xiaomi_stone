06f609f70e531177acaceb7e77338191c884a40f
kamikaonashi <thorschi93@gmx.at>
2025-01-18 14:52:00 +0100

    stone: remove lineage health

--- device.mk
+++ device.mk
@@ -421,12 +421,6 @@ PRODUCT_COPY_FILES += \
     frameworks/native/data/etc/android.hardware.vulkan.version-1_1.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.vulkan.version-1_1.xml \
     frameworks/native/data/etc/android.software.vulkan.deqp.level-2020-03-01.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.software.vulkan.deqp.level.xml
 
-# Lineage Health
-ifneq ($(wildcard hardware/*/interfaces/health),)
-PRODUCT_PACKAGES += \
-    vendor.lineage.health-service.default
-endif
-
 # Power
 PRODUCT_PACKAGES += \
     android.hardware.power@1.2.vendor \
